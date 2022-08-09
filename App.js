4f30905897c8ccfc1b188ce875f5df2e66efbd86
Tom Frenois <tom.frenois@epita.fr>
2022-08-09 18:58:19 +0200

    add header

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Alert,
+  Dimensions,
+} from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+var appHeight = Dimensions.get('window').height
+var appWidth = Dimensions.get('window').width
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header />
+    </SafeAreaView>
   );
 }
 
+// COMPONENTS
+const Header = () => {
+  return(
+    <View style={styles.header}>
+      <Text style={styles.headerItem} onPress={handleClickMenu} >
+        <FontAwesome5 name={'bars'} color={'white'} size={32} />
+      </Text>
+      <Text style={styles.headerItem}>
+        Ma Liste de Film
+      </Text>
+      <Text style={styles.headerItem} onPress={handleClickSetting}>
+        <FontAwesome5 name={'cog'} color={'white'} size={32} />
+      </Text>
+    </View>
+  )
+}
+
+
+// FUNCTIONS
+const handleClickMenu = () => {
+  Alert.alert('Menu Button Clicked')
+}
+
+const handleClickSetting = () => {
+  Alert.alert('Settings Button Clicked')
+}
+
+
+// STYLES
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#403d3d',
   },
+  header:{
+    backgroundColor: '#1f1d1d',
+    width: appWidth,
+    height: appHeight / 9,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  headerItem: {
+    paddingTop: 45,
+    paddingStart: 10,
+    paddingEnd: 10,
+    color: 'white',
+    fontSize: 30
+  }
 });
